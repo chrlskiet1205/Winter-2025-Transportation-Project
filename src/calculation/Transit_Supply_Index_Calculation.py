6717336641352655ba315c76f2add4e8ec0a412d
local-Dune Nguyen <dunghc58@gmail.com>
2026-01-06 11:51:12 -0800

    Added functions and Python script for calculating transit supply index

--- src/calculation/Transit_Supply_Index_Calculation.py
+++ src/calculation/Transit_Supply_Index_Calculation.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import os
-from standardize_function import z_calculation
+from utils.standardize_function import z_calculation
+from utils.index_function import index_calculation
+
+def calculating_per_capita(data:pd.Series, population_data:pd.Series) -> pd.Series:
+    result_lst = []
+
+    for i in range(len(data)):
+        result_lst.append(data[i]/population_data[i])
+    
+    return pd.Series(result_lst)
+
 
 input_folder = 'data/processed'
 output_folder = 'data/processed'
@@ -8,4 +18,28 @@ output_folder = 'data/processed'
 processed_dataset = 'finalized.csv'
 input_path = os.path.join(input_folder, processed_dataset)
 
-supply_index = pd.read_csv(input_path)
+supply_index_df = pd.read_csv(input_path)
+
+#Calculating per capita values
+uza_service_population = supply_index_df['Service_Area_Pop']
+VRM_per_capita: pd.Series = calculating_per_capita(supply_index_df['VRM'], uza_service_population)
+VRH_per_capita: pd.Series = calculating_per_capita(supply_index_df['VRH'], uza_service_population)
+UPT_per_capita: pd.Series = calculating_per_capita(supply_index_df['UPT'], uza_service_population)
+
+#Calculating mean values
+VRM_mean = VRM_per_capita.mean()
+VRH_mean = VRH_per_capita.mean()
+UPT_mean = UPT_per_capita.mean()
+
+#Calculating std
+VRM_std = VRM_per_capita.std()
+VRH_std = VRH_per_capita.std()
+UPT_std = UPT_per_capita.std()
+
+#Calculating supply index for each UZAs
+VRM_z = z_calculation(VRM_per_capita, VRM_mean, VRM_std)
+VRH_z = z_calculation(VRH_per_capita, VRH_mean, VRH_std)
+UPT_z = z_calculation(UPT_per_capita, UPT_mean, UPT_std)
+
+supply_index = index_calculation(VRM_z, VRH_z, UPT_z)
+print(UPT_z)
--- src/calculation/standardize_function.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def z_calculation(data:pd.Series, mean, std) -> list:
-    return_lst = []
-
-    for x in data:
-        return_lst.append((x-mean)/std)
-        
-    return return_lst
